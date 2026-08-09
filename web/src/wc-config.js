6b198b6a38c89d7b4375809cbb38fd7bcba869c8
Ayomide <Excellency001-boop@users.noreply.github.com>
2026-08-10 00:54:18 +0100

    Turn on WalletConnect: phone users can now pick any wallet and connect

--- web/src/wc-config.js
+++ web/src/wc-config.js
@@ -1,4 +1,4 @@
 // Your free WalletConnect (Reown) Project ID goes here.
 // Get it at https://cloud.reown.com. It is a public key, safe to commit.
 // While this is empty, the app just uses the installed-wallet picker as before.
-export const WALLETCONNECT_PROJECT_ID = "";
+export const WALLETCONNECT_PROJECT_ID = "4e5bd5459891ac8c2157eb6b0b505c00";
